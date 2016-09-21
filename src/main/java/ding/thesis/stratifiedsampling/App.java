34d8d757dc451ff7efb5f1b409dae1bd76811ffd
ding <ding@192.168.8.104>
2016-09-21 21:55:31 +0300

    my first commit
    Signed-off-by: ding <ding@192.168.8.104>

---
+++ src/main/java/ding/thesis/stratifiedsampling/App.java
@@ -0,0 +1,13 @@
+package ding.thesis.stratifiedsampling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
